8fde72b4d0381bd54a75667d701ef71510175500
jalxby <jalxby@gmail.com>
2023-09-09 19:57:30 +0200

    fix: storybook styling + dark mode

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,15 +1,23 @@
 import type { Preview } from '@storybook/react'
-
+import { themes } from '@storybook/theming'
+import '@/app/styles/index.scss'
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/,
+        color: /(background|color)$/i,
+        date: /Date$/,
       },
     },
+    darkMode: {
+      dark: { ...themes.dark, appBg: '#171717' },
+      light: { ...themes.normal, appBg: '#808080' },
+    },
+    docs: {
+      theme: themes.dark,
+    },
   },
-};
+}
 
-export default preview;
+export default preview
