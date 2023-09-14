82f7a658bb9544d802b0a0a37ea0d62cac2bfe50
Alex <rubanovv.alex@gmail.com>
2023-09-14 20:54:58 +0200

    fix: modified text-field --setpredefined default state/props

--- src/ui/text-field/model/text-field-types.ts
+++ src/ui/text-field/model/text-field-types.ts
@@ -3,7 +3,7 @@ import { ComponentPropsWithoutRef } from 'react'
 import { INPUT_TYPES } from '../lib/constants/input-type-enum'
 
 export type InputProps = {
-  label: string
-  inputType: (typeof INPUT_TYPES)[keyof typeof INPUT_TYPES]
+  label?: string
+  inputType?: (typeof INPUT_TYPES)[keyof typeof INPUT_TYPES]
   error?: string
 } & Omit<ComponentPropsWithoutRef<'input'>, 'type'>
