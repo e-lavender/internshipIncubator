44a12e2566d862a60d3f8a036b0da4f42b6845a6
Alex <rubanovv.alex@gmail.com>
2023-09-14 12:10:11 +0200

    fix: modified label & checkbox components

--- src/ui/label/label.tsx
+++ src/ui/label/label.tsx
@@ -6,13 +6,13 @@ import { Typography } from '../typography/typography'
 
 type LabelProps = {
   id?: string
-  labelTitle?: ReactNode
+  title?: string
 } & ComponentPropsWithoutRef<'label'>
 
-export const Label: FC<PropsWithChildren<LabelProps>> = ({ id, labelTitle, children }) => (
+export const Label: FC<PropsWithChildren<LabelProps>> = ({ id, title, children }) => (
   <>
     <LabelRDX.Root htmlFor={id}>
-      <Typography variant="regular-16">{labelTitle}</Typography>
+      {title && <Typography variant="regular-14">{title}</Typography>}
 
       {children}
     </LabelRDX.Root>
