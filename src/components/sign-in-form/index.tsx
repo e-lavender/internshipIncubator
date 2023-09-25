27dee33c9db9a4e587a0283fa4749dc8bbea8b81
Yelkira <penda.fu@gmail.com>
2023-09-25 23:19:16 +0300

    fix: fix sign-in form

--- src/components/sign-in-form/index.tsx
+++ src/components/sign-in-form/index.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { FC } from 'react'
+
+import { DevTool } from '@hookform/devtools'
 
 import s from './sign-in-form.module.scss'
 
 import { GithubIcon, GoogleIcon } from '@/app/assets/svg'
+import { ControlledTextField } from '@/components/controlled/controlled-input'
+import { LoginFormType, useSignInForm } from '@/components/sign-in-form/use-sign-in-form'
 import { Button } from '@/ui/button'
 import { Card } from '@/ui/card'
-import { TextField } from '@/ui/text-field'
 import { Typography } from '@/ui/typography/typography'
 
-export const SignIn = () => {
+type PropsType = {
+  onSubmitHandler?: (data: LoginFormType) => void
+}
+export const SignIn: FC<PropsType> = ({ onSubmitHandler }) => {
+  const { handleSubmit, control } = useSignInForm()
+
+  const onSubmit = handleSubmit(data => onSubmitHandler!(data))
+
   return (
     <Card className={s.wrapper}>
       <Typography variant={'h1'}>Sign In</Typography>
@@ -20,13 +30,19 @@ export const SignIn = () => {
           <GithubIcon width={36} height={36} />
         </button>
       </div>
-      <form className={s.form} action="">
-        <TextField placeholder={'User name'} label={'Email'} />
-        <TextField placeholder={'Password'} label={'Password'} />
+      <form className={s.form} onSubmit={onSubmit}>
+        <DevTool control={control} />
+        <ControlledTextField label={'Email'} name={'email'} control={control} />
+        <ControlledTextField
+          label={'Password'}
+          name={'password'}
+          inputType={'password'}
+          control={control}
+        />
         <Typography className={s.forgotPassword} variant={'regular-14'}>
           Forgot password
         </Typography>
-        <Button className={s.signInBtn} fullWidth={true}>
+        <Button type={'submit'} className={s.signInBtn} fullWidth={true}>
           Sign In
         </Button>
         <Typography className={s.accountText} variant={'regular-16'}>
