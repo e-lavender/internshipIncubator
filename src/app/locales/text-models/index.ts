13c85c176b5d10bd04ce3b57427c65f634a4d1f1
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2023-09-28 19:34:21 +0300

    Update index.ts

--- src/app/locales/text-models/index.ts
+++ src/app/locales/text-models/index.ts
@@ -3,4 +3,4 @@ import NavigationTextModel from './navigation-text-model.json'
 import PrivatyPolicyTextModel from './privaty-text-model.json'
 import TermsTextModel from './terms-text-model.json'
 
-export { NavigationTextModel, CharacterPageTextModel, TermsTextModel, PrivatyPolicyTextModel }
+export {  TermsTextModel, PrivatyPolicyTextModel }
