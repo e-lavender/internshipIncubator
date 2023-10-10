3f2f0cf3341dcd339b2cfc6c288ff75d4dc0d8d7
Alex <rubanovv.alex@gmail.com>
2023-10-11 01:08:50 +0200

    fix: fixed storybook redux problem, added providers in stories

--- src/components/sign-in-form/sign-in-form.stories.tsx
+++ src/components/sign-in-form/sign-in-form.stories.tsx
@@ -1,5 +1,7 @@
 import { Meta, StoryObj } from '@storybook/react'
+import { Provider } from 'react-redux'
 
+import { store } from '@/app/store/store'
 import { SignInForm } from '@/components'
 
 const meta = {
@@ -9,9 +11,11 @@ const meta = {
   args: {},
   decorators: [
     Story => (
-      <div style={{ margin: '3em', display: 'flex', justifyContent: 'center' }}>
-        <Story />
-      </div>
+      <Provider store={store}>
+        <div style={{ margin: '3em', display: 'flex', justifyContent: 'center' }}>
+          <Story />
+        </div>
+      </Provider>
     ),
   ],
   argTypes: {},
