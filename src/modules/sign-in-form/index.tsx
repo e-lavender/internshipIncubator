20ed1dfe7525fc04a40b11a0be3794359649d32e
Yevhen <yevhenchornenko@gmail.com>
2023-10-15 16:42:31 +0300

    fix: fix SignIN

--- src/modules/sign-in-form/index.tsx
+++ src/modules/sign-in-form/index.tsx
@@ -1,6 +1,7 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 
 import { DevTool } from '@hookform/devtools'
+import { LinearProgress } from '@mui/joy'
 import Link from 'next/link'
 import { toast } from 'react-toastify'
 
@@ -8,18 +9,17 @@ import s from './sign-in-form.module.scss'
 
 import { authNavigationUrls, useTranslation, useSignInMutation } from '@/app'
 import { ControlledTextField } from '@/components/text-field-controlled/controlled-text-field'
-import { LoginFormType, useSignInForm } from '@/modules/sign-in-form/use-sign-in-form'
+import { useSignInForm } from '@/modules/sign-in-form/use-sign-in-form'
 import { Button, Card, GithubButton, GoogleButton, Typography } from '@/ui'
 
-type PropsType = {
-  onSubmitHandler?: (data: LoginFormType) => void
-}
-export const SignInForm: FC<PropsType> = () => {
-  const { handleSubmit, control } = useSignInForm()
+export const SignInForm = () => {
+  const [isSignIn, setIsSignIn] = useState<boolean>(false)
+  const { handleSubmit, setFocus, control } = useSignInForm()
   const [signIn] = useSignInMutation()
   const { t } = useTranslation()
   const { signInForm: text } = t.authPages.signInPage
   const onSubmitForm = handleSubmit(data => {
+    setIsSignIn(true)
     signIn(data)
       .unwrap()
       .then(() => {
@@ -28,46 +28,65 @@ export const SignInForm: FC<PropsType> = () => {
       .catch(error => {
         toast.error(error.data.message)
       })
+      .finally(() => {
+        setIsSignIn(false)
+      })
   })
 
-  return (
-    <Card className={s.wrapper}>
-      <Typography variant={'h1'}>{text.signIn}</Typography>
-      <div className={s.oauthIcons}>
-        <GoogleButton />
-        <GithubButton />
-      </div>
-      <form className={s.form} onSubmit={onSubmitForm}>
-        <DevTool control={control} />
-        <ControlledTextField label={text.email} name={'email'} control={control} />
-        <ControlledTextField
-          className={s.textField}
-          label={text.password}
-          name={'password'}
-          inputType={'password'}
-          control={control}
-        />
-        <Link href={authNavigationUrls.forgotPassword()}>
-          <Typography className={s.forgotPassword} variant={'regular-14'}>
-            {text.forgotPassword}
-          </Typography>
-        </Link>
-        <Button type={'submit'} className={s.signInBtn} fullWidth={true}>
-          {text.signIn}
-        </Button>
-        <Typography className={s.accountText} variant={'regular-16'}>
-          {text.haveAccount}
-        </Typography>
+  useEffect(() => {
+    setFocus('email')
+  }, [])
 
-        <Button
-          className={s.signUpBtn}
-          as={Link}
-          variant={'link'}
-          href={authNavigationUrls.signUp()}
-        >
-          {text.signUp}
-        </Button>
-      </form>
-    </Card>
+  return (
+    <div>
+      <Card className={s.container}>
+        <div className={s.progressBar}>
+          {isSignIn && <LinearProgress thickness={3} color={'neutral'} />}
+        </div>
+        <form className={s.form} onSubmit={onSubmitForm}>
+          <div className={s.wrapper}>
+            <Typography variant={'h1'}>{text.signIn}</Typography>
+            <div className={s.oauthIcons}>
+              <GoogleButton />
+              <GithubButton />
+            </div>
+            <DevTool control={control} />
+            <ControlledTextField
+              className={s.textField}
+              label={text.email}
+              name={'email'}
+              control={control}
+            />
+            <ControlledTextField
+              className={s.textField}
+              label={text.password}
+              name={'password'}
+              inputType={'password'}
+              control={control}
+            />
+            <Link href={authNavigationUrls.forgotPassword()}>
+              <Typography className={s.forgotPassword} variant={'regular-14'}>
+                {text.forgotPassword}
+              </Typography>
+            </Link>
+            <Button disabled={isSignIn} type={'submit'} className={s.signInBtn} fullWidth={true}>
+              {text.signIn}
+            </Button>
+            <Typography className={s.accountText} variant={'regular-16'}>
+              {text.haveAccount}
+            </Typography>
+
+            <Button
+              className={s.signUpBtn}
+              as={Link}
+              variant={'link'}
+              href={authNavigationUrls.signUp()}
+            >
+              {text.signUp}
+            </Button>
+          </div>
+        </form>
+      </Card>
+    </div>
   )
 }
