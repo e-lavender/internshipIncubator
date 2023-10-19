cbeeb263c7585d4bf5e00daf166855a1c75b1ba5
Yevhen <yevhenchornenko@gmail.com>
2023-10-19 20:39:53 +0300

    fix: fix MatchMedia hook hydration

--- src/app/hooks/useMatchMedia.ts
+++ src/app/hooks/useMatchMedia.ts
@@ -1,40 +1,44 @@
 import { useState, useLayoutEffect } from 'react'
 
+import { MatchMedia } from '@/app'
+
 const queries = [
   '(min-width: 320px) and (max-width: 400px)',
   '(min-width: 767px) and (max-width: 1199px)',
   '(min-width: 1200px)',
 ]
 
-interface MediaQueries {
-  isMobile: boolean
-  isTablet: boolean
-  isDesktop: boolean
-}
-
-export const useMatchMedia = (): MediaQueries => {
-  const isClient = typeof window === 'object'
-
-  const mediaQueryLists = queries.map(query => (isClient ? window.matchMedia(query) : null))
-  const getValues = () => mediaQueryLists.map(list => (list ? list.matches : false))
-  const [values, setValues] = useState<any>(getValues)
+export const useMatchMedia = (): MatchMedia => {
+  const isClient = typeof window !== 'undefined'
+  const [values, setValues] = useState<MatchMedia>({
+    isMobile: false,
+    isTablet: false,
+    isDesktop: false,
+  })
 
   useLayoutEffect(() => {
-    if (isClient) {
-      const handler = () => setValues(getValues)
-
-      mediaQueryLists.forEach(list => (list ? list.addEventListener('change', handler) : null))
-
-      return () =>
-        mediaQueryLists.forEach(list => (list ? list.removeEventListener('change', handler) : null))
+    const getValues = () => {
+      return {
+        isMobile: window.matchMedia(queries[0]).matches,
+        isTablet: window.matchMedia(queries[1]).matches,
+        isDesktop: window.matchMedia(queries[2]).matches,
+      }
     }
-  }, [])
 
-  return ['isMobile', 'isTablet', 'isDesktop'].reduce(
-    (acc, screen, index) => ({
-      ...acc,
-      [screen]: values[index],
-    }),
-    {} as MediaQueries
-  )
+    const updateValues = () => {
+      setValues(getValues())
+    }
+
+    if (isClient) {
+      updateValues()
+
+      window.addEventListener('resize', updateValues)
+
+      return () => {
+        window.removeEventListener('resize', updateValues)
+      }
+    }
+  }, [isClient])
+
+  return values
 }
