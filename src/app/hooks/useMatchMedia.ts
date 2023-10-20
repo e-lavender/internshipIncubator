a3ebc1f1a4ab8914e5085738ee0218dc4cacb06c
Yevhen <yevhenchornenko@gmail.com>
2023-10-20 21:10:37 +0300

    fix: add throttle

--- src/app/hooks/useMatchMedia.ts
+++ src/app/hooks/useMatchMedia.ts
@@ -2,11 +2,13 @@ import { useState, useLayoutEffect } from 'react'
 
 import { MatchMedia } from '@/app'
 
-const queries = [
-  '(min-width: 320px) and (max-width: 420px)',
-  '(min-width: 421px) and (max-width: 1199px)',
-  '(min-width: 1200px)',
-]
+const QUERIES = {
+  MOBILE: '(min-width: 320px) and (max-width: 420px)',
+  TABLET: '(min-width: 421px) and (max-width: 1199px)',
+  DESKTOP: '(min-width: 1200px)',
+}
+
+const THROTTLED_TIME = 500
 
 export const useMatchMedia = (): MatchMedia => {
   const isClient = typeof window !== 'undefined'
@@ -19,9 +21,9 @@ export const useMatchMedia = (): MatchMedia => {
   useLayoutEffect(() => {
     const getValues = () => {
       return {
-        isMobile: window.matchMedia(queries[0]).matches,
-        isTablet: window.matchMedia(queries[1]).matches,
-        isDesktop: window.matchMedia(queries[2]).matches,
+        isMobile: window.matchMedia(QUERIES.MOBILE).matches,
+        isTablet: window.matchMedia(QUERIES.TABLET).matches,
+        isDesktop: window.matchMedia(QUERIES.DESKTOP).matches,
       }
     }
 
@@ -29,13 +31,31 @@ export const useMatchMedia = (): MatchMedia => {
       setValues(getValues())
     }
 
-    if (isClient) {
-      updateValues()
+    let timer: number
 
-      window.addEventListener('resize', updateValues)
+    const throttledUpdate = () => {
+      if (!timer) {
+        // Гарантированный первый вызов
+        updateValues()
+
+        timer = window.setTimeout(() => {
+          timer = 0
+          updateValues()
+        }, THROTTLED_TIME)
+      }
+    }
+
+    if (isClient) {
+      throttledUpdate()
+
+      const handleResize = () => {
+        throttledUpdate()
+      }
+
+      window.addEventListener('resize', handleResize)
 
       return () => {
-        window.removeEventListener('resize', updateValues)
+        window.removeEventListener('resize', handleResize)
       }
     }
   }, [isClient])
