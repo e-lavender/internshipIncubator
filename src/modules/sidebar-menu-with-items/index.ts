64d6986d54ec9835495ce10a76427994d4c018f4
Alex <rubanovv.alex@gmail.com>
2023-10-28 03:24:59 +0200

    feat: added mobile version of sidebar menu

--- src/modules/sidebar-menu-with-items/index.ts
+++ src/modules/sidebar-menu-with-items/index.ts
@@ -1,1 +1,2 @@
 export * from './sidebar-menu-with-items'
+export * from './mobile'
--- src/templates/layouts/sidebar-menu-layout/sidebar-menu-layout.tsx
+++ src/templates/layouts/sidebar-menu-layout/sidebar-menu-layout.tsx
@@ -1,16 +1,23 @@
 import { PropsWithChildren } from 'react'
 
+import { clsx } from 'clsx'
+
 import s from './sidebar-menu-layout.module.scss'
 
-import { SidebarMenuWithItems } from '@/modules'
+import { useMatchMedia } from '@/app'
+import { SidebarMenuWithItems, MobileSidebarMenuWithItems } from '@/modules'
 import { HeaderLayout } from '@/templates/layouts/header-layout'
 
 export const SidebarMenuLayout = ({ children }: PropsWithChildren) => {
+  const { isMobile } = useMatchMedia()
+
+  const styles = clsx(!isMobile && s.wrapper)
+
   return (
     <>
       <HeaderLayout />
-      <div className={s.wrapper}>
-        <SidebarMenuWithItems />
+      <div className={styles}>
+        {isMobile ? <MobileSidebarMenuWithItems /> : <SidebarMenuWithItems />}
         <div className={s.content}>{children}</div>
       </div>
     </>
