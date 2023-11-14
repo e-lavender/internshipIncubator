469ac2988e5fe0277c2c51b0a1b62d8f33a6f508
Alex <rubanovv.alex@gmail.com>
2023-11-14 01:52:26 +0100

    feat: added stories for radio group --storybook + added styles for disabled state of group/item

--- src/ui/radio-group/radio-container/radio-container.tsx
+++ src/ui/radio-group/radio-container/radio-container.tsx
@@ -1,6 +1,7 @@
 import { PropsWithChildren } from 'react'
 
 import * as RadioSelect from '@radix-ui/react-radio-group'
+import { clsx } from 'clsx'
 
 import s from './radio-container.module.scss'
 
@@ -16,13 +17,16 @@ type RadioContainerProps = {
 }
 export const RadioContainer = ({
   defaultValue,
+  disabled,
   children,
   ...props
 }: PropsWithChildren<RadioContainerProps>) => {
+  const styles = clsx(s.root, disabled && s.disabled)
+
   return (
     <form>
       <RadioSelect.Root
-        className={s.root}
+        className={styles}
         defaultValue={defaultValue}
         aria-label="View density"
         {...props}
--- src/ui/radio-group/radio-item/radio-item.tsx
+++ src/ui/radio-group/radio-item/radio-item.tsx
@@ -1,6 +1,7 @@
 import { useId } from 'react'
 
 import * as RadioSelect from '@radix-ui/react-radio-group'
+import { clsx } from 'clsx'
 
 import s from './radio-item.module.scss'
 
@@ -12,13 +13,19 @@ type RadioItemProps = {
   label?: string
 }
 
-export const RadioItem = ({ value, label }: RadioItemProps) => {
+export const RadioItem = ({ value, label, disabled }: RadioItemProps) => {
   const id = useId()
 
+  const styles = {
+    root: clsx(s.wrapper, disabled && s.disabled),
+    item: clsx(s.item, disabled && s.disabled),
+    indicator: clsx(s.indicator, disabled && s.disabled),
+  }
+
   return (
-    <div className={s.wrapper}>
-      <RadioSelect.Item className={s.item} value={value} id={id}>
-        <RadioSelect.Indicator className={s.indicator} />
+    <div className={styles.root}>
+      <RadioSelect.Item className={styles.item} value={value} id={id} disabled={disabled}>
+        <RadioSelect.Indicator className={styles.indicator} />
       </RadioSelect.Item>
       {label && <Label title={label} id={id} />}
     </div>
