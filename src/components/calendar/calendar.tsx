d5c220cd2bf2473fd7b5735c9515a1bd02da080b
Yevhen <yevhenchornenko@gmail.com>
2023-11-20 12:59:15 +0200

    fix: add locales

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -1,30 +1,11 @@
 import React, { PropsWithChildren, useEffect, useState } from 'react'
 
-import en, { getMonth, getYear } from 'date-fns'
-import DatePicker, { registerLocale } from 'react-datepicker'
+import { getMonth, getYear } from 'date-fns'
+import DatePicker from 'react-datepicker'
 import './react-datepicker.scss'
 
-import ru from 'date-fns/locale/ru'
-import eng from 'date-fns/locale/eng'
-
-import { CalendarIcon, NextIcon, PreviousIcon } from '@/app'
+import { CalendarIcon, NextIcon, PreviousIcon, useTranslation } from '@/app'
 import { Typography } from '@/ui'
-registerLocale('en', en)
-registerLocale('ru', ru)
-const MONTH = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
-]
 
 type calendarProps = {
   isRange?: boolean
@@ -38,6 +19,8 @@ export const Calendar = ({
   const [isYearPiker, setIsYearPiker] = useState(false)
   const [dateRange, setDateRange] = useState([null, null])
   const [startDateInRange, endDateInRange] = dateRange
+  const { t } = useTranslation()
+  const { month, locale } = t.calendar
 
   useEffect(() => {
     setIsMonthPiker(false)
@@ -63,14 +46,15 @@ export const Calendar = ({
         selected={startDate}
         shouldCloseOnSelect={!isMonthPiker && !isYearPiker}
         showYearPicker={isYearPiker}
-        locale="ru"
-        //dateFormat="yyyy"
+        locale={locale}
         showMonthYearPicker={isMonthPiker}
         startDate={isRange ? startDateInRange : null}
         endDate={isRange ? endDateInRange : null}
         selectsRange={isRange}
         dayClassName={date => (date.getDay() === 0 || date.getDay() === 6 ? 'weekend' : null)}
-        onChange={date => (isRange ? setDateRange(date) : setStartDate(date))}
+        onChange={(date: Date | [Date | null, Date | null] | null) => {
+          isRange ? setDateRange(date as null[]) : setStartDate(date as Date)
+        }}
         renderCustomHeader={({
           date,
           decreaseMonth,
@@ -92,7 +76,7 @@ export const Calendar = ({
             <div>
               <button onClick={handleMonthPiker}>
                 <Typography className="react-datepicker__navigation--month" variant="bold-16">
-                  {MONTH[getMonth(date)]}
+                  {month[getMonth(date)]}
                 </Typography>
               </button>
               <button onClick={handleYearPick}>
