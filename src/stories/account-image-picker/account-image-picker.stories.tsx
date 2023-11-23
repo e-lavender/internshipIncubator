b25599d4a072c4fc07220fdb9e05e1420430e448
Alex <rubanovv.alex@gmail.com>
2023-11-23 18:27:01 +0100

    feat: added story for AccountImagePicker --storybook

---
+++ src/stories/account-image-picker/account-image-picker.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { AccountImagePicker } from '@/modules'
+
+const meta = {
+  title: 'Module/AccountImagePicker',
+  component: AccountImagePicker,
+  args: {},
+  decorators: [
+    Story => (
+      <div style={{ margin: '3em' }}>
+        <Story />
+      </div>
+    ),
+  ],
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof AccountImagePicker>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {}
