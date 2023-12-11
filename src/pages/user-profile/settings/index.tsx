9ef8478fe48fee72b10a99b4ecb93088bd1597e8
Alex <rubanovv.alex@gmail.com>
2023-12-11 21:27:34 +0100

    feat: added structure of general settings screen + modified TextField component props/styles(added required prop that helps to inform user that field is required)

--- src/pages/user-profile/settings/index.tsx
+++ src/pages/user-profile/settings/index.tsx
@@ -5,18 +5,36 @@ import s from './general-information.module.scss'
 import { Calendar } from '@/components'
 import { AccountImagePicker } from '@/modules'
 import { ProfileSettingLayout } from '@/templates'
+import { Button, Select, TextArea, TextField } from '@/ui'
 
 const GeneralInformation = () => {
   return (
-    <>
-      <h1 style={{ textAlign: 'center', margin: '10rem 0 5rem' }}>General Information</h1>
-      <div className={s.container}>
-        <AccountImagePicker />
+    <div className={s.container}>
+      <div className={s.content}>
+        <div className={s.image}>
+          <AccountImagePicker />
+        </div>
+
         <div className={s.wrapper}>
+          <TextField label={'Username'} required />
+          <TextField label={'First Name'} required />
+          <TextField label={'Last Name'} required />
+
           <Calendar />
+
+          <div className={s.select}>
+            <Select value={'-'} onChange={console.log} options={[]} label={'Select your country'} />
+            <Select value={'-'} onChange={console.log} options={[]} label={'Select your city'} />
+          </div>
+
+          <TextArea label={'About me'} placeholder={'Please provide information here...'} />
         </div>
       </div>
-    </>
+
+      <div className={s.divider}></div>
+
+      <Button className={s.btn}>Save Changes</Button>
+    </div>
   )
 }
 
