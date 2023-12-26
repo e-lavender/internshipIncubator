8a83cd24abf7394fc2801b76242946d8762b5c70
jalxby <jalxby@gmail.com>
2023-12-26 14:42:58 +0100

    INC-105 fix: general settings form fields length restrictions, date picker border radius on hover

---
+++ src/middleware.ts
@@ -0,0 +1,5 @@
+// middleware.ts
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { toast } from 'react-toastify'
+export function middleware(request: Request) {}
