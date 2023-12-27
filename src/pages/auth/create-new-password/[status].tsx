68b44c6559ef54a6da662c0c3d0b4f156a087e86
jalxby <jalxby@gmail.com>
2023-12-27 08:43:55 +0100

    Merge pull request #109
    * INC-105 fix: tab index datepicker & select
    
    * INC-105 fix: imports, added sharp package for optimize images in prod…
    
    * INC-105 fix: date picker border radius
    
    * INC-105 fix: general settings form fields length restrictions, date p…
    
    * INC-105 fix: general settings form fields length restrictions, date p…
    
    * INC-105 fix: refactoring
    
    * Merge remote-tracking branch 'origin/develop' into INC-105
    
    * INC-105 feat: protecting routes logic, implement utils for working wi…
    
    * INC-105 fix: password restore flow

--- src/pages/auth/create-new-password/[status].tsx
+++ src/pages/auth/create-new-password/[status].tsx
@@ -3,28 +3,33 @@ import React from 'react'
 import Error from 'next/error'
 import { useRouter } from 'next/router'
 
-import { LinkConfirmed, LinkExpired } from '@/modules'
+import { LinkExpired, NewPasswordForm } from '@/modules'
 
 const NewPasswordCodeStatus = {
   success: 'success',
   expired: 'expired',
 } as const
 
-type QueryType = { status: ConfirmationStatusType }
+type QueryType = {
+  status: ConfirmationStatusType
+  code: string
+}
 type ConfirmationStatusType = keyof typeof NewPasswordCodeStatus
 
-const LinkStatusData: {
-  [key in ConfirmationStatusType]: React.ReactElement
-} = {
-  [NewPasswordCodeStatus.success]: <LinkConfirmed />,
-  [NewPasswordCodeStatus.expired]: <LinkExpired />,
+const withRecoveryCode = (params: { status: ConfirmationStatusType; code?: string }) => {
+  const { status, code } = params
+
+  return {
+    [NewPasswordCodeStatus.success]: <NewPasswordForm code={code} />,
+    [NewPasswordCodeStatus.expired]: <LinkExpired />,
+  }[status]
 }
 
 const CreateNewPassword = () => {
   const { query } = useRouter()
   const typedQuery = query as QueryType
 
-  return LinkStatusData[typedQuery.status] || <Error statusCode={404} />
+  return withRecoveryCode({ ...typedQuery }) || <Error statusCode={404} />
 }
 
 export default CreateNewPassword
