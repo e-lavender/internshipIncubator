65eb06d071a059a93d52b607c17e78e92365ab51
Alex <rubanovv.alex@gmail.com>
2024-01-01 18:41:02 +0100

    fix: problem with initial render of profile settings(general information screen)

--- src/app/helpers/set-date-format.ts
+++ src/app/helpers/set-date-format.ts
@@ -5,15 +5,8 @@ const setDateTemplate = (day: string, month: string, year: string) => {
 export const setDateFormat = (date: string | Date | undefined): string => {
   if (date == undefined) return ''
 
-  if (typeof date === 'string') {
-    const newDate = new Date(date).toLocaleDateString()
-    const [day, month, year] = newDate.split('.')
-
-    return setDateTemplate(day, month, year)
-  }
-
-  const dateToString = date?.toLocaleDateString()
-  const [day, month, year] = dateToString.split('.')
+  const newDate = new Date(date).toLocaleDateString()
+  const [day, month, year] = newDate.split('.')
 
   return setDateTemplate(day, month, year)
 }
