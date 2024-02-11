03be99697668c289d2f4cfb3973695e6e4479e8c
jalxby <jalxby@gmail.com>
2024-02-11 13:31:51 +0100

    INC-134 fix: env, images remotePatterns, added package to generate openApi

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 /** @type {{reactStrictMode: boolean, i18n: {defaultLocale: string, locales: string[], localeDetection: boolean}}} */
 const nextConfig = {
+  env: {
+    BASE_API_URL: 'https://inctagram.work',
+    DEVELOPMENT_FRONT_BASE_URL: 'http://localhost:3000',
+    PRODUCTION_FRONT_BASE_URL: 'https://freedomindz.site',
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'storage.yandexcloud.net',
+        pathname: '/users-inctagram/users/**',
+      },
+    ],
+  },
   reactStrictMode: true,
   i18n: {
     locales: ['en', 'ru'],
