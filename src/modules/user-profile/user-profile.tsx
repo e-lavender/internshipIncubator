87a6982c311a27cb56392a88ee51a7eaf2c9f7a0
jalxby <jalxby@gmail.com>
2024-02-26 06:35:10 +0100

    INC-137 fix: binding url to the user`s post

--- src/modules/user-profile/user-profile.tsx
+++ src/modules/user-profile/user-profile.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { useRouter } from 'next/router'
 
 import s from './user-profile.module.scss'
 
 import { useGetMeQuery } from '@/app/services/auth/auth.api'
+import { usePostCardModal } from '@/app/services/modals/modals.hooks'
 import { useGetPublicUserProfileByIdQuery } from '@/app/services/profile/profile.api'
 import { PublicUserModel, UserProfileModel } from '@/app/services/profile/profile.api.types'
+import { useGetPublicPostByIdQuery } from '@/app/services/public-posts/public-posts.api'
 import { UserProfileGallery } from '@/components'
 import { UserProfileDescription } from '@/modules'
 
@@ -19,10 +21,19 @@ export const UserProfile = () => {
   const { data: user } = useGetMeQuery()
   const { query } = useRouter()
   const profileId = Number(query.id?.[0])
+  const postId = Number(query.id?.[1])
 
   const isMyProfile = user?.userId === profileId
-
+  const { data: postById } = useGetPublicPostByIdQuery({ postId }, { skip: !postId })
   const { data: publicUser } = useGetPublicUserProfileByIdQuery({ profileId })
+  const { openPostCardModal, setPostCardModalSelectedPost } = usePostCardModal()
+
+  useEffect(() => {
+    if (postId && postById) {
+      setPostCardModalSelectedPost(postById)
+      openPostCardModal()
+    }
+  }, [postId])
 
   return (
     <main className={s.container}>
